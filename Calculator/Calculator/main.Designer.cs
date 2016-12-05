6b05555c916214d840014d3164bd78f711807900
Minkyu Kim <external@naraeon.net>
2016-12-05 19:07:49 +0900

    * Commit for design parts

---
+++ Calculator/Calculator/main.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Calculator
+{
+    partial class form_main
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.list_history = new System.Windows.Forms.ListBox();
+            this.text_result = new System.Windows.Forms.TextBox();
+            this.button_num7 = new System.Windows.Forms.Button();
+            this.button_num9 = new System.Windows.Forms.Button();
+            this.button_num8 = new System.Windows.Forms.Button();
+            this.button_num5 = new System.Windows.Forms.Button();
+            this.button_num6 = new System.Windows.Forms.Button();
+            this.button_num4 = new System.Windows.Forms.Button();
+            this.button_num2 = new System.Windows.Forms.Button();
+            this.button_num3 = new System.Windows.Forms.Button();
+            this.button_num1 = new System.Windows.Forms.Button();
+            this.button_dot = new System.Windows.Forms.Button();
+            this.button_num0 = new System.Windows.Forms.Button();
+            this.button_add = new System.Windows.Forms.Button();
+            this.button_sub = new System.Windows.Forms.Button();
+            this.button_mul = new System.Windows.Forms.Button();
+            this.button_div = new System.Windows.Forms.Button();
+            this.button_inv = new System.Windows.Forms.Button();
+            this.button_pct = new System.Windows.Forms.Button();
+            this.button_equ = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // list_history
+            // 
+            this.list_history.Font = new System.Drawing.Font("굴림", 12F);
+            this.list_history.FormattingEnabled = true;
+            this.list_history.ItemHeight = 20;
+            this.list_history.Location = new System.Drawing.Point(9, 11);
+            this.list_history.Name = "list_history";
+            this.list_history.Size = new System.Drawing.Size(307, 124);
+            this.list_history.TabIndex = 0;
+            // 
+            // text_result
+            // 
+            this.text_result.Font = new System.Drawing.Font("굴림", 12F);
+            this.text_result.Location = new System.Drawing.Point(9, 140);
+            this.text_result.Name = "text_result";
+            this.text_result.ReadOnly = true;
+            this.text_result.Size = new System.Drawing.Size(307, 30);
+            this.text_result.TabIndex = 1;
+            // 
+            // button_num7
+            // 
+            this.button_num7.Font = new System.Drawing.Font("굴림", 12F);
+            this.button_num7.Location = new System.Drawing.Point(9, 176);
+            this.button_num7.Name = "button_num7";
+            this.button_num7.Size = new System.Drawing.Size(55, 66);
+            this.button_num7.TabIndex = 2;
+            this.button_num7.Text = "7";
+            this.button_num7.UseVisualStyleBackColor = true;
+            // 
+            // button_num9
+            // 
+            this.button_num9.Font = new System.Drawing.Font("굴림", 12F);
+            this.button_num9.Location = new System.Drawing.Point(131, 176);
+            this.button_num9.Name = "button_num9";
+            this.button_num9.Size = new System.Drawing.Size(55, 66);
+            this.button_num9.TabIndex = 3;
+            this.button_num9.Text = "9";
+            this.button_num9.UseVisualStyleBackColor = true;
+            // 
+            // button_num8
+            // 
+            this.button_num8.Font = new System.Drawing.Font("굴림", 12F);
+            this.button_num8.Location = new System.Drawing.Point(70, 176);
+            this.button_num8.Name = "button_num8";
+            this.button_num8.Size = new System.Drawing.Size(55, 66);
+            this.button_num8.TabIndex = 4;
+            this.button_num8.Text = "8";
+            this.button_num8.UseVisualStyleBackColor = true;
+            // 
+            // button_num5
+            // 
+            this.button_num5.Font = new System.Drawing.Font("굴림", 12F);
+            this.button_num5.Location = new System.Drawing.Point(70, 247);
+            this.button_num5.Name = "button_num5";
+            this.button_num5.Size = new System.Drawing.Size(55, 66);
+            this.button_num5.TabIndex = 7;
+            this.button_num5.Text = "5";
+            this.button_num5.UseVisualStyleBackColor = true;
+            // 
+            // button_num6
+            // 
+            this.button_num6.Font = new System.Drawing.Font("굴림", 12F);
+            this.button_num6.Location = new System.Drawing.Point(131, 247);
+            this.button_num6.Name = "button_num6";
+            this.button_num6.Size = new System.Drawing.Size(55, 66);
+            this.button_num6.TabIndex = 6;
+            this.button_num6.Text = "6";
+            this.button_num6.UseVisualStyleBackColor = true;
+            // 
+            // button_num4
+            // 
+            this.button_num4.Font = new System.Drawing.Font("굴림", 12F);
+            this.button_num4.Location = new System.Drawing.Point(9, 248);
+            this.button_num4.Name = "button_num4";
+            this.button_num4.Size = new System.Drawing.Size(55, 66);
+            this.button_num4.TabIndex = 5;
+            this.button_num4.Text = "4";
+            this.button_num4.UseVisualStyleBackColor = true;
+            // 
+            // button_num2
+            // 
+            this.button_num2.Font = new System.Drawing.Font("굴림", 12F);
+            this.button_num2.Location = new System.Drawing.Point(70, 319);
+            this.button_num2.Name = "button_num2";
+            this.button_num2.Size = new System.Drawing.Size(55, 66);
+            this.button_num2.TabIndex = 10;
+            this.button_num2.Text = "2";
+            this.button_num2.UseVisualStyleBackColor = true;
+            // 
+            // button_num3
+            // 
+            this.button_num3.Font = new System.Drawing.Font("굴림", 12F);
+            this.button_num3.Location = new System.Drawing.Point(131, 319);
+            this.button_num3.Name = "button_num3";
+            this.button_num3.Size = new System.Drawing.Size(55, 66);
+            this.button_num3.TabIndex = 9;
+            this.button_num3.Text = "3";
+            this.button_num3.UseVisualStyleBackColor = true;
+            // 
+            // button_num1
+            // 
+            this.button_num1.Font = new System.Drawing.Font("굴림", 12F);
+            this.button_num1.Location = new System.Drawing.Point(9, 319);
+            this.button_num1.Name = "button_num1";
+            this.button_num1.Size = new System.Drawing.Size(55, 66);
+            this.button_num1.TabIndex = 8;
+            this.button_num1.Text = "1";
+            this.button_num1.UseVisualStyleBackColor = true;
+            // 
+            // button_dot
+            // 
+            this.button_dot.Font = new System.Drawing.Font("굴림", 12F);
+            this.button_dot.Location = new System.Drawing.Point(131, 391);
+            this.button_dot.Name = "button_dot";
+            this.button_dot.Size = new System.Drawing.Size(55, 66);
+            this.button_dot.TabIndex = 12;
+            this.button_dot.Text = ".";
+            this.button_dot.UseVisualStyleBackColor = true;
+            // 
+            // button_num0
+            // 
+            this.button_num0.Font = new System.Drawing.Font("굴림", 12F);
+            this.button_num0.Location = new System.Drawing.Point(9, 391);
+            this.button_num0.Name = "button_num0";
+            this.button_num0.Size = new System.Drawing.Size(116, 66);
+            this.button_num0.TabIndex = 11;
+            this.button_num0.Text = "0";
+            this.button_num0.UseVisualStyleBackColor = true;
+            // 
+            // button_add
+            // 
+            this.button_add.Font = new System.Drawing.Font("굴림", 12F);
+            this.button_add.Location = new System.Drawing.Point(192, 391);
+            this.button_add.Name = "button_add";
+            this.button_add.Size = new System.Drawing.Size(55, 66);
+            this.button_add.TabIndex = 16;
+            this.button_add.Text = "+";
+            this.button_add.UseVisualStyleBackColor = true;
+            // 
+            // button_sub
+            // 
+            this.button_sub.Font = new System.Drawing.Font("굴림", 12F);
+            this.button_sub.Location = new System.Drawing.Point(192, 319);
+            this.button_sub.Name = "button_sub";
+            this.button_sub.Size = new System.Drawing.Size(55, 66);
+            this.button_sub.TabIndex = 15;
+            this.button_sub.Text = "-";
+            this.button_sub.UseVisualStyleBackColor = true;
+            // 
+            // button_mul
+            // 
+            this.button_mul.Font = new System.Drawing.Font("굴림", 12F);
+            this.button_mul.Location = new System.Drawing.Point(192, 247);
+            this.button_mul.Name = "button_mul";
+            this.button_mul.Size = new System.Drawing.Size(55, 66);
+            this.button_mul.TabIndex = 14;
+            this.button_mul.Text = "*";
+            this.button_mul.UseVisualStyleBackColor = true;
+            // 
+            // button_div
+            // 
+            this.button_div.Font = new System.Drawing.Font("굴림", 12F);
+            this.button_div.Location = new System.Drawing.Point(192, 176);
+            this.button_div.Name = "button_div";
+            this.button_div.Size = new System.Drawing.Size(55, 66);
+            this.button_div.TabIndex = 13;
+            this.button_div.Text = "/";
+            this.button_div.UseVisualStyleBackColor = true;
+            // 
+            // button_inv
+            // 
+            this.button_inv.Font = new System.Drawing.Font("굴림", 12F);
+            this.button_inv.Location = new System.Drawing.Point(253, 248);
+            this.button_inv.Name = "button_inv";
+            this.button_inv.Size = new System.Drawing.Size(63, 66);
+            this.button_inv.TabIndex = 18;
+            this.button_inv.Text = "1/x";
+            this.button_inv.UseVisualStyleBackColor = true;
+            // 
+            // button_pct
+            // 
+            this.button_pct.Font = new System.Drawing.Font("굴림", 12F);
+            this.button_pct.Location = new System.Drawing.Point(253, 176);
+            this.button_pct.Name = "button_pct";
+            this.button_pct.Size = new System.Drawing.Size(63, 66);
+            this.button_pct.TabIndex = 17;
+            this.button_pct.Text = "%";
+            this.button_pct.UseVisualStyleBackColor = true;
+            // 
+            // button_equ
+            // 
+            this.button_equ.Font = new System.Drawing.Font("굴림", 12F);
+            this.button_equ.Location = new System.Drawing.Point(253, 320);
+            this.button_equ.Name = "button_equ";
+            this.button_equ.Size = new System.Drawing.Size(63, 137);
+            this.button_equ.TabIndex = 19;
+            this.button_equ.Text = "=";
+            this.button_equ.UseVisualStyleBackColor = true;
+            this.button_equ.Click += new System.EventHandler(this.button_equ_Click);
+            // 
+            // form_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 466);
+            this.Controls.Add(this.button_equ);
+            this.Controls.Add(this.button_inv);
+            this.Controls.Add(this.button_pct);
+            this.Controls.Add(this.button_add);
+            this.Controls.Add(this.button_sub);
+            this.Controls.Add(this.button_mul);
+            this.Controls.Add(this.button_div);
+            this.Controls.Add(this.button_dot);
+            this.Controls.Add(this.button_num0);
+            this.Controls.Add(this.button_num2);
+            this.Controls.Add(this.button_num3);
+            this.Controls.Add(this.button_num1);
+            this.Controls.Add(this.button_num5);
+            this.Controls.Add(this.button_num6);
+            this.Controls.Add(this.button_num4);
+            this.Controls.Add(this.button_num8);
+            this.Controls.Add(this.button_num9);
+            this.Controls.Add(this.button_num7);
+            this.Controls.Add(this.text_result);
+            this.Controls.Add(this.list_history);
+            this.Name = "form_main";
+            this.Text = "계산기";
+            this.Load += new System.EventHandler(this.form_main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox list_history;
+        private System.Windows.Forms.TextBox text_result;
+        private System.Windows.Forms.Button button_num7;
+        private System.Windows.Forms.Button button_num9;
+        private System.Windows.Forms.Button button_num8;
+        private System.Windows.Forms.Button button_num5;
+        private System.Windows.Forms.Button button_num6;
+        private System.Windows.Forms.Button button_num4;
+        private System.Windows.Forms.Button button_num2;
+        private System.Windows.Forms.Button button_num3;
+        private System.Windows.Forms.Button button_num1;
+        private System.Windows.Forms.Button button_dot;
+        private System.Windows.Forms.Button button_num0;
+        private System.Windows.Forms.Button button_add;
+        private System.Windows.Forms.Button button_sub;
+        private System.Windows.Forms.Button button_mul;
+        private System.Windows.Forms.Button button_div;
+        private System.Windows.Forms.Button button_inv;
+        private System.Windows.Forms.Button button_pct;
+        private System.Windows.Forms.Button button_equ;
+    }
+}
+
---
+++ Calculator/Calculator/main.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    enum State
+    {
+        STATE_NOTHING, //입력되지 않음
+        STATE_LHS, //좌측 수 입력 완료
+        STATE_OPERATOR, //좌측 수 & 연산자 입력 완료
+    };
+
+    enum Operator
+    {
+        OPERATOR_NOTHING, //입력되지 않음
+        OPERATOR_ADD, // +
+        OPERATOR_SUB, // -
+        OPERATOR_MUL, // *
+        OPERATOR_DIV  // /
+    }
+
+    public partial class form_main : Form
+    {
+        private int lhs = 0, rhs = 0;
+        private State current_state = State.STATE_NOTHING;
+        private Operator current_operator = Operator.OPERATOR_NOTHING;
+
+        public form_main()
+        {
+            InitializeComponent();
+        }
+
+        private void button_equ_Click(object sender, EventArgs e)
+        {
+            // 입력된 lhs와 current_operator를 바탕으로 답 계산
+            // lhs만 있는 경우 lhs 출력
+            // 연산 완료 후 각 변수들 초기 상태로 되돌림
+        }
+
+        private void form_main_Load(object sender, EventArgs e)
+        {
+            //창이 크기 조정에 대응하지 못하므로 사이즈 고정
+            MinimumSize = new Size(Width, Height);
+            MaximumSize = new Size(Width, Height);
+        }
+    }
+}
